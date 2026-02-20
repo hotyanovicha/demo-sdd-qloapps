6ac547b8496b9e2929916b0c6b7904bf3b8386c3
hotyanovicha <hotyanovicha@gmail.com>
2026-02-20 16:42:28 +0100

    refactor: Update Playwright service configuration to use `createAzurePlaywrightConfig` and `connectTimeout`.

--- playwright.service.config.ts
+++ playwright.service.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from '@playwright/test';
-import { getServiceConfig } from '@azure/playwright';
+import { createAzurePlaywrightConfig } from '@azure/playwright';
 import { DefaultAzureCredential } from '@azure/identity';
 import config from './playwright.config';
 
 export default defineConfig(
   config,
-  getServiceConfig({
+  createAzurePlaywrightConfig(config, {
     exposeNetwork: '<loopback>',
-    timeout: 30000,
+    connectTimeout: 30000,
     os: 'linux',
-    useCloudHostedBrowsers: false,
     credential: new DefaultAzureCredential(),
   }),
   {
