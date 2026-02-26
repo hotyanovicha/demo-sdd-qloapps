248e1b70d1991be971388e9af9ca5d153304b90f
hotyanovicha <hotyanovicha@gmail.com>
2026-02-26 13:37:49 +0100

    feat: Refactor user registration logic into a separate function for better reusability

--- src/ui/fixtures/index.ts
+++ src/ui/fixtures/index.ts
@@ -1,4 +1,4 @@
-import { test as base } from '@playwright/test';
+import { test as base, Browser, BrowserContext } from '@playwright/test';
 import { PageManager } from '@pages/page-manager';
 import { createRegistrationData } from '@src/ui/test-data/factories/user.factory';
 import { RegistrationData } from '@types-ui/index';
@@ -16,31 +16,39 @@ type CustomFixtures = {
   };
 };
 
+async function registerUser(
+  browser: Browser,
+): Promise<{ pages: PageManager; user: RegistrationData; context: BrowserContext }> {
+  const user = createRegistrationData();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const pages = new PageManager(page);
+
+  await pages.homePage.open();
+  await pages.homePage.waitForLoad();
+  await pages.homePage.goToSignIn();
+  await pages.authPage.waitForLoad();
+  await pages.authPage.createAccount(user.email);
+  await pages.registrationPage.waitForLoad();
+  await pages.registrationPage.register(user);
+  await pages.myAccountPage.waitForLoad();
+
+  return { pages, user, context };
+}
+
 export const test = base.extend<CustomFixtures>({
   pages: async ({ page }, use) => {
     await use(new PageManager(page));
   },
 
   authPages: async ({ browser }, use) => {
-    const user = createRegistrationData();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    const authPage = new PageManager(page);
-
-    await authPage.homePage.open();
-    await authPage.homePage.waitForLoad();
-    await authPage.homePage.goToSignIn();
-    await authPage.authPage.waitForLoad();
-    await authPage.authPage.createAccount(user.email);
-    await authPage.registrationPage.waitForLoad();
-    await authPage.registrationPage.register(user);
-    await authPage.myAccountPage.waitForLoad();
+    const { pages: authPage, user, context } = await registerUser(browser);
     await use({ authPage, user });
     await context.close();
   },
 
-  checkoutSummaryPage: async ({ authPages }, use) => {
-    const { authPage, user } = authPages;
+  checkoutSummaryPage: async ({ browser }, use) => {
+    const { pages: authPage, user, context } = await registerUser(browser);
     const { checkInDay, checkOutDay } = getSearchDates();
 
     await authPage.homePage.open();
@@ -57,6 +65,7 @@ export const test = base.extend<CustomFixtures>({
     const totalPrice = await authPage.checkoutPage.getTotalPrice();
 
     await use({ authPage, user, roomName, totalPrice });
+    await context.close();
   },
 });
 
